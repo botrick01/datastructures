310154f69399ad516bcea299a5d9b6d0610be398
botrick01 <johannsenpatrick.privat@gmail.com>
2022-09-23 15:33:04 +0200

    stack added

---
+++ Stack/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class CustomStack<T>
+    {
+        public List<T> Stack = new List<T>();
+
+        public T push(T item)
+        {
+            Stack.Add(item);
+            return item;
+        }
+        public T pop()
+        {
+            T item = Stack[Stack.Count];
+            Stack.RemoveAt(Stack.Count);
+            return item;
+        }
+        public int size()
+        {
+            return Stack.Count;
+        }
+    }
+}
