9df8307b0a3fa9632194f4d7ea24b8b3fb3af54d
botrick01 <johannsenpatrick.privat@gmail.com>
2022-11-18 09:56:29 +0100

    strategy pattern added for singlelinkedlist

---
+++ StrategyPattern/IMyList.cs
@@ -0,0 +1,59 @@
+﻿using LinkedList;
+
+namespace Common
+{
+    public interface IMyList
+    {
+        void SwitchNodes(Node argFirstNode, Node argSecondNode);
+        void SetSortStrategy(SortStrategy sortStrategy);
+        Node GetFirst();
+        void Sort();
+    }
+
+    public abstract class SortStrategy
+    {
+        public abstract void Sort(IMyList list);
+    }
+
+    public class InsertionSort : SortStrategy
+    {
+        public override void Sort(IMyList list)
+        {
+            Node currentNode = list.GetFirst().nextNode;
+            while (currentNode != null)
+            {
+                for (var cur = list.GetFirst(); cur != null; cur = cur.nextNode)
+                {
+                    if (cur == currentNode)
+                        break;
+                    if ((int)cur.Value < (int)currentNode.Value)
+                        continue;
+                    (cur.Value, currentNode.Value) = (currentNode.Value, cur.Value);
+                }
+
+                currentNode = currentNode.nextNode;
+            }
+        }
+    }
+
+    public class InsertionSortReverse : SortStrategy
+    {
+        public override void Sort(IMyList list)
+        {
+            Node currentNode = list.GetFirst().nextNode;
+            while (currentNode != null)
+            {
+                for (var cur = list.GetFirst(); cur != null; cur = cur.nextNode)
+                {
+                    if (cur == currentNode)
+                        break;
+                    if ((int)cur.Value > (int)currentNode.Value)
+                        continue;
+                    (cur.Value, currentNode.Value) = (currentNode.Value, cur.Value);
+                }
+
+                currentNode = currentNode.nextNode;
+            }
+        }
+    }
+}
--- UnitTest/SingleLinkedListTest.cs
+++ UnitTest/SingleLinkedListTest.cs
@@ -1,3 +1,4 @@
+using Common;
 using LinkedList;
 using NUnit.Framework;
 
@@ -173,12 +174,30 @@ namespace UnitTest
             linkedList.InsertLast(13);
             linkedList.InsertLast(8);
             linkedList.InsertLast(2);
-            linkedList.InsertionSortReverse();
-            var node = linkedList.head;
+            linkedList.SetSortStrategy(new InsertionSortReverse());
+            linkedList.Sort();
+            var node = linkedList.GetFirst();
             Assert.AreEqual(13, node.Value);
             Assert.AreEqual(8, node.nextNode.Value);
             Assert.AreEqual(4, node.nextNode.nextNode.Value);
             Assert.AreEqual(1, node.nextNode.nextNode.nextNode.nextNode.Value);
+        }
+        [Test]
+        public void TestInsertionSort_Sort_ListIsInCorrectOrder()
+        {
+            LinkedList.LinkedList linkedList = new LinkedList.LinkedList();
+            linkedList.InsertLast(4);
+            linkedList.InsertLast(1);
+            linkedList.InsertLast(13);
+            linkedList.InsertLast(8);
+            linkedList.InsertLast(2);
+            linkedList.SetSortStrategy(new InsertionSort());
+            linkedList.Sort();
+            var node = linkedList.GetFirst();
+            Assert.AreEqual(1, node.Value);
+            Assert.AreEqual(2, node.nextNode.Value);
+            Assert.AreEqual(4, node.nextNode.nextNode.Value);
+            Assert.AreEqual(13, node.nextNode.nextNode.nextNode.nextNode.Value);
         }
     }
 }
