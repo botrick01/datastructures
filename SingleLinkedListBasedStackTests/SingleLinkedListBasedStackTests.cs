87093f77152be084065a2f29d14d3b8647841dbb
botrick01 <johannsenpatrick.privat@gmail.com>
2022-12-02 12:37:29 +0100

    StackTests complete

--- SingleLinkedListBasedStackTests/SingleLinkedListBasedStackTests.cs
+++ SingleLinkedListBasedStackTests/SingleLinkedListBasedStackTests.cs
@@ -11,9 +11,25 @@ namespace SingleLinkedListBasedStackTests
         }
 
         [Test]
-        public void Push_5Nodes_PushedCorrect()
+        public void Push_ThreeElements_StackOrderOK()
         {
-            SingleLinkedListBasedStack.SingleLinkedListBasedStack stack = new SingleLinkedListBasedStack();
+            SingleLinkedListBasedStack.SingleLinkedListBasedStack stack = new SingleLinkedListBasedStack.SingleLinkedListBasedStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(stack.ToString(), "1\n2\n3");
+        }
+
+        [Test]
+        public void PushAndPop_PushThreeTimesPoPonce_TwoElementsInStackLastOneRemoved()
+        {
+            SingleLinkedListBasedStack.SingleLinkedListBasedStack stack = new SingleLinkedListBasedStack.SingleLinkedListBasedStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            var last = stack.Pop();
+            Assert.AreEqual(last.Value, 3);
+            Assert.AreEqual(stack.ToString(), "1\n2");
         }
     }
 }
